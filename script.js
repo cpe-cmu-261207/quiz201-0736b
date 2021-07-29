9e62fb1beb300c75ba3c9896ca5b129eecf9233d
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-07-29 13:53:07 +0700

    author calculation

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ const color = document.querySelector('#color')
 
 // define more constants and variables here
 
+const text = document.querySelector('#text');
+const btn_reset = document.querySelector('#reset');
+const btn_search = document.querySelector('#search');
+
+
 btn_toggle.onclick = () => {
   // your code here
+  calculation.innerHTML = 630610736 + parseInt(length.value);
+  if(author.style.display === "none"){
+    author.style.display = "block";
+    calculation.style.display = "none";
+    btn_toggle.innerHTML = "Show Calculation";
+  }
+  else{
+    author.style.display = "none";
+    calculation.style.display = "block";
+    btn_toggle.innerHTML = "Show Author";
+  }
 }
 
 // more codes for Search and Reset buttons here
