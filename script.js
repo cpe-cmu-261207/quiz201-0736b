ffba7830082bfd62efaad12239e57ba85d6c9fe6
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-07-29 14:08:57 +0700

    added search reset

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const text = document.querySelector('#text');
 const btn_reset = document.querySelector('#reset');
 const btn_search = document.querySelector('#search');
 
+//backup original paragraph text for Reset
+const tempText = text.innerHTML;
 
 btn_toggle.onclick = () => {
   // your code here
@@ -27,3 +29,42 @@ btn_toggle.onclick = () => {
 }
 
 // more codes for Search and Reset buttons here
+
+//Cut the word push in array
+const wordCut = (str) => {
+  let arrText = [];
+  const word = str.split(" ");
+  arrText.push(word);
+  return arrText;
+}
+
+// Add Span to word that over length
+const textOverLength = () => {
+  const textCutted = wordCut(tempText);
+  for(let i = 0; i < 312; i++){
+    if(textCutted[0][i].length >= length.value){
+      textCutted[0][i] = "<span style='color:"+ `${color.value}` + "'>" + textCutted[0][i] + "</span>";
+    }
+  }
+  console.log(textCutted);
+  return textCutted;
+}
+
+btn_search.onclick = () => {
+  const addedSpan = textOverLength();
+  let textWithSpan = "";
+  if(textWithSpan.length >= 0){
+    textWithSpan = "";
+  }
+  for(let i = 0; i < 312; i++){
+    textWithSpan += `${addedSpan[0][i]}` + " ";
+  }
+  console.log(textWithSpan);
+  text.innerHTML = textWithSpan;
+}
+
+btn_reset.onclick = () => {
+  length.value = "5";
+  color.value ="#FF0000";
+  text.innerHTML = tempText;
+}
